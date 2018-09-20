65d1ccfe1e8e810f15e56a8a506bbc2b19f1e23d
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-20 23:49:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,19 +3,32 @@ katz_deli = []
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    message = "The line is currently:"
+    message = "The line is currently: "
     
   array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
+    message += " #{index + 1}. #{value}"
   end 
   puts "#{message}"
   end 
 end 
- def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
- end
+
+#   def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+# end
  
+ 
+def take_a_number(katz_deli)
+  i = 1 
+  katz_deli << i 
+  puts "Welcome, your ticket number is #{i}, and you are #{katz_deli.length} in line"
+  i + 1 
+end
+
+take_a_number([])
+take_a_number([1])
+
+
  
  def now_serving(katz_deli)
    if katz_deli.length == 0
@@ -25,3 +38,5 @@ end
      katz_deli.shift()
    end
  end
+ 
+ 
